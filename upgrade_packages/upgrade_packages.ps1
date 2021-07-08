120277ee856e7fcdc286f29038bc9d188180fa24
Harsh Pandey <akionsight@gmail.com>
2021-07-08 22:06:47 +0530

    Fix typo

--- upgrade_packages/upgrade_packages.ps1
+++ upgrade_packages/upgrade_packages.ps1
@@ -2,6 +2,6 @@ echo "starting upgrade"
 
 wsl sudo bash update_packages.sh
 
-echo "`nScript:this window will now automactically close in 5 seconds"
+echo "`nScript:this window will now automatically close in 5 seconds"
 
 Start-Sleep -s 5
