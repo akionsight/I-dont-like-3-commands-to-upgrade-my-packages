f6b127286a5d4d8f78527a47ba7e1e6d68ce0333
AkIonSight <akionsight@gmail.com>
2022-07-14 22:36:06 +0530

    Change apt-get to apt and add apt autoremove

--- upgrade_packages/update_packages.sh
+++ upgrade_packages/update_packages.sh
@@ -1,19 +1,24 @@
 echo -e "\nStarting Update\n"
 
-apt-get update
+apt update
 
 echo -e "\nrevised package lists of arrived\n"
 
 echo -e "\nupgrading packages\n"
 
-apt-get upgrade
+apt upgrade
 
 echo -e "\nUpgrading Flatpaks \n"
 
 flatpak upgrade
+
+echo -e "\n Removing no longer needed packages \n"
+
+apt autoremove
+
 echo -e "\nPackages have been upgraded, cleaning used package lists\n"
 
-apt-get clean
+apt clean
 
 echo -e "Done\n"
 
